c3e7b1fa3abf91a271f2e6c19286cb823cf9ffdc
JJ-ammaz <julian.jjammaz@gmail.com>
2026-02-11 10:30:32 -0500

    so far on tank (movement basics left to right with logs and bounding boxes)

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,7 +1,18 @@
+using System.Threading;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Move : MonoBehaviour
 {
+    Vector2 bottoml;
+    Vector2 topr;
+    //use this maybe? public bool flipped = true;
+
+    float speed = 5f;
+    float duration = 2f;
+    private bool leftcondition = false;
+    private bool rightcondition = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +22,56 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.right * Time.deltaTime;
+        
+        //Disallow movement outside border rudementary
+        Vector3 newPosition = transform.position;
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        if (screenPos.x < 0 || screenPos.x > Screen.width)
+        {
+            speed = speed * -1;
+        }
+
+
+        //input movement
+        if (Keyboard.current.leftArrowKey.wasPressedThisFrame)
+        {
+            leftcondition = true;
+        }
+
+
+        if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
+        {
+            rightcondition = true;
+        }
+
+
+        //transform.position += transform.right * Time.deltaTime;
+        if (Keyboard.current.leftArrowKey.wasReleasedThisFrame)
+        {
+            Debug.Log("left release");
+            leftcondition = false;
+            //print("hello dead")
+        }
+
+
+        if (Keyboard.current.rightArrowKey.wasReleasedThisFrame)
+        {
+            Debug.Log("right release");
+            rightcondition = false;
+        }
+
+
+        if (leftcondition == true)
+        {
+            Debug.Log("left condition");
+            newPosition.x += -speed * Time.deltaTime;
+        }
+        if (rightcondition == true)
+        {
+            Debug.Log("right condition");
+            newPosition.x += speed * Time.deltaTime;
+        }
+
+        transform.position = newPosition;
     }
 }
